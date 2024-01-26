83ee16c48e93a5915862572a0ea3f2326352f733
monger18 <satyamsri1809@gmail.com>
2024-01-26 16:23:29 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Register from './RegisterAndLoginForm'
+import axios from 'axios'
+import { UserContextProvider, UserContext } from './UserContext'
+import Routes from './Routes'
+
+function App() {
+  axios.defaults.baseURL = 'http://localhost:4000'
+  axios.defaults.withCredentials = true
+  return (
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
+  )
+}
+
+export default App
